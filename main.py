6b5ce92c2871e9635eee82f844963c481ff8e010
dominicfollett <dominic.follett.smith@gmail.com>
2017-12-15 10:29:46 +0200

    Add basic oauth.

--- main.py
+++ main.py
@@ -13,14 +13,59 @@
 # 1. Install Python dependencies: cv2, flask. (wish that pip install works like a charm)
 # 2. Run "python main.py".
 # 3. Navigate the browser to the local webpage.
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, redirect, request, session, url_for, jsonify
 from camera import VideoCamera
+from flask_oauthlib.client import OAuth
 
 app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development-secret'
+oauth = OAuth(app)
+
+github = oauth.remote_app(
+    'github',
+    consumer_key='6761c48074347fb52a23',
+    consumer_secret='bb9b033f40b9ff33cf217cf1741923cfd4905115',
+    request_token_params={'scope': 'user:email'},
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize'
+)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'github_token' in session:
+        me = github.get('user')
+        return render_template('index.html')
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return github.authorize(callback=url_for('authorized', _external=True))
+
+@app.route('/logout')
+def logout():
+    session.pop('github_token', None)
+    return redirect(url_for('index'))
+
+@app.route('/login/authorized')
+def authorized():
+    resp = github.authorized_response()
+    if resp is None or resp.get('access_token') is None:
+        return 'Access denied: reason=%s error=%s resp=%s' % (
+            request.args['error'],
+            request.args['error_description'],
+            resp
+        )
+    session['github_token'] = (resp['access_token'], '')
+    me = github.get('user')
+    return redirect(url_for('index'))
+
+@github.tokengetter
+def get_github_oauth_token():
+    return session.get('github_token')
 
 def gen(camera):
     while True:
@@ -34,4 +79,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, threaded=True)
