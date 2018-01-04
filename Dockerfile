162b67b4665921956c54313fdf70d3eacfe3655b
dominicfollett <dominic.follett.smith@gmail.com>
2018-01-04 10:06:35 +0200

    Add dlib without mkl support. Try face_reconition package.

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,29 @@ MAINTAINER Nate Johnson <nate@biobright.org>
 
 # GStreamer  and openCV deps
 # Several retries is a workaround for flaky downloads
-RUN packages="python3-picamera curl python python-dev unzip supervisor libzmq3 libzmq3-dev v4l-utils python3-pip python3-dev python3-numpy python-numpy libgstreamer1.0-dev libgstreamer1.0-0 libgstreamer-vaapi1.0-dev libgstreamer-vaapi1.0-0 libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev libgstreamer-plugins-bad1.0-0 libgstreamer-plugins-base1.0-0 gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-omx build-essential cmake libeigen3-dev libjpeg-dev libtiff5-dev libtiff5 libjasper-dev libjasper1 libpng12-dev libpng12-0 libavcodec-dev  libavcodec56 libavformat-dev libavformat56 libswscale-dev libswscale3 libv4l-dev libv4l-0 libatlas-base-dev libatlas3-base gfortran libblas-dev libblas3 liblapack-dev liblapack3 python3-dev libpython3-dev" \
+RUN packages="zip software-properties-common pkg-config libgtk2.0-dev libatlas-dev libgraphicsmagick1-dev graphicsmagick wget git libboost-all-dev python3-picamera curl python python-dev unzip supervisor libzmq3 libzmq3-dev v4l-utils python3-pip python3-dev python3-numpy python-numpy libgstreamer1.0-dev libgstreamer1.0-0 libgstreamer-vaapi1.0-dev libgstreamer-vaapi1.0-0 libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev libgstreamer-plugins-bad1.0-0 libgstreamer-plugins-base1.0-0 gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-omx build-essential cmake libeigen3-dev libjpeg-dev libtiff5-dev libtiff5 libjasper-dev libjasper1 libpng12-dev libpng12-0 libavcodec-dev  libavcodec56 libavformat-dev libavformat56 libswscale-dev libswscale3 libv4l-dev libv4l-0 libatlas-base-dev libatlas3-base gfortran libblas-dev libblas3 liblapack-dev liblapack3 python3-dev libpython3-dev" \
     && apt-get -y update \
     && apt-get -y install $packages \
     || apt-get -y install $packages \
     || apt-get -y install $packages \
     || apt-get -y install $packages \
+    || apt-get -y install $packages \
+    || apt-get -y install $packages \
+    || apt-get -y install $packages \
     || apt-get -y install $packages
+
+# Build Dlib without MKL
+RUN cd /tmp \
+    && git clone https://github.com/davisking/dlib.git \
+    && cd dlib \
+    && mkdir build \
+    && cd build \
+    && cmake .. -DDLIB_USE_CUDA=0 -DUSE_AVX_INSTRUCTIONS=1 -DDLIB_USE_MKL_FFT=0 -DLIB_USE_MKL_FFT_STR=0 -DLIB_USE_BLAS=0 -DLIB_USE_LAPACK=0 \
+    && cmake --build . \
+    && cd .. \
+    && python3 setup.py install --yes USE_AVX_INSTRUCTIONS --no DLIB_USE_CUDA --no DLIB_USE_MKL_FFT --no DLIB_USE_MKL_FFT_STR --no DLIB_USE_BLAS --no DLIB_USE_LAPACK \
+    && cd /
+    && rm -rf /tmp/*dlib*
 
 # We might consider installing pip, pip3, pip numpy here
 # if it provides any performance/bug fixes
--- main.py
+++ main.py
@@ -24,6 +24,7 @@ import imutils
 import time
 import cv2
 import os
+import face_recognition
 
 CLIENT_ID = os.environ.get('CLIENT_ID')
 CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
@@ -31,6 +32,14 @@ CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 if CLIENT_ID is None or CLIENT_SECRET is None:
     print("Exiting: Oauth client ID and secret is not set in the environment...")
     exit(0)
+
+# TODO load all SPAN faces here.
+# Load a sample picture and learn how to recognize it.
+obama_image = face_recognition.load_image_file("./lib/obama.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+dom_image = face_recognition.load_image_file("./lib/dom.jpg")
+dom_face_encoding = face_recognition.face_encodings(dom_image)[0]
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -104,19 +113,61 @@ def get_github_oauth_token():
     return session.get('github_token')
 
 def gen(vs):
+    PROCESS_FRAME = True
     # loop over the frames from the video stream
     while True:
         # grab the frame from the threaded video stream and resize it
         # to have a maximum width of 400 pixels
         frame = vs.read()
-        frame = imutils.resize(frame, width=500)
+        #frame = imutils.resize(frame, width=500)
+        
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-        for (x,y,w,h) in faces:
-            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-            # Send frame off for recognition.
+        #for (x,y,w,h) in faces:
+        #    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        #    # Send frame off for recognition.
+
+        if PROCESS_FRAME:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                match = face_recognition.compare_faces([obama_face_encoding, dom_face_encoding], face_encoding)
+                name = "Unknown"
+
+                if match[0]:
+                    name = "Barack"
+                if match[1]:
+                    name = "Dom"
+
+                face_names.append(name)
+
+        PROCESS_FRAME = not PROCESS_FRAME
+
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
         # draw the timestamp on the frame
         timestamp = datetime.datetime.now()
