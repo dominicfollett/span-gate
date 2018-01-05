3e79347bef8446743169fa2b9f0b679041a48bb6
dominicfollett <dominic.follett.smith@gmail.com>
2018-01-05 08:39:52 +0200

    Try different frame rate and resolution.

--- stream.py
+++ stream.py
@@ -13,7 +13,7 @@ class VideoStream:
 			from pivideostream import PiVideoStream
             print("Initializing the camera.")
 			# initialize the picamera stream and allow the camera sensor to warmup. 
-			self.stream = PiVideoStream((640, 480), 30)
+			self.stream = PiVideoStream(resolution=(640, 480), framerate=50)
 
 		# otherwise, we are using OpenCV so initialize the webcam
 		# stream
